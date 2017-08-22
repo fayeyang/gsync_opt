52c613fb82dd90c2ad523268b3c7b28509b043a2
faye <faye@163.com>
2017-08-22 09:15:48 +0800

    current init

--- gsync_opt.sh
+++ gsync_opt.sh
@@ -1,24 +1,10 @@
 #!/bin/bash
 
-ROW=`wc -l < gsync.log`
+ROW=`wc -l < /home/faye/work/gsync/gsync.log`
 echo $ROW
-ROW=0
 while read LINE ; do
-    echo $LINE
-    ((ROW=ROW+1))
-    if [ $ROW -eq 10 ]; then
-        ls -al /proc/self/fd
-        break
-    fi
-done < /home/faye/gsync.log
-
-ls -al /proc/self/fd
-
-#echo -e "`ps`"
-#str=($(echo -e "`ps -o pid,comm`" | grep opt.sh))
-#echo ${str[*]}
-
-#fuser -um /home/faye/gsync.log
+    :
+done < /home/faye/work/gsync/gsync.log
 
 
 
